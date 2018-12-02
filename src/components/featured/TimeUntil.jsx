4ecfe957cbcd4de03b343b38eea4c49aa4dc08d4
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2018-12-02 20:56:53 +0300

    Count Down implemented

--- src/components/featured/TimeUntil.jsx
+++ src/components/featured/TimeUntil.jsx
@@ -1,30 +1,56 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
 
 class TimeUntil extends Component {
-  state = {};
-  render() {
-    var item = {
-      name: '',
-      value: 0
-    };
-    var items = [('days', 59), ('weks', 3)];
+  state = {
+    deadline: 'Dec, 20, 2019',
+    days: '0',
+    hours: '0',
+    minutes: '0',
+    seconds: '0'
+  };
 
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    if (time < 0) {
+      console.log('Date passed');
+    } else {
+      const seconds = Math.floor((time / 1000) % 60);
+      const minutes = Math.floor((time / 1000 / 60) % 60);
+      const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+      this.setState({ days, hours, minutes, seconds });
+    }
+  }
+  componentDidMount() {
+    setInterval(() => this.getTimeUntil(this.state.deadline), 1000);
+  }
+
+  render() {
     return (
-      <div className='countdown_wrapper'>
-        <div className='countdown_top'>Event Starts In</div>
-        <div className='countdown_bottom'>
-          {items.map(key => (
-            <div key={key} className='countdown_item'>
-              <div key={key} className='countdown_time'>
-                59
-              </div>
-              <div key={key + 1} className='countdown_tag'>
-                {key}
-              </div>
+      <Fade left delay={1000}>
+        <div className='countdown_wrapper'>
+          <div className='countdown_top'>Event Starts In</div>
+          <div className='countdown_bottom'>
+            <div className='countdown_item'>
+              <div className='countdown_time'>{this.state.days}</div>
+              <div className='countdown_tag'>days</div>
             </div>
-          ))}
+            <div className='countdown_item'>
+              <div className='countdown_time'>{this.state.hours}</div>
+              <div className='countdown_tag'>hr</div>
+            </div>
+            <div className='countdown_item'>
+              <div className='countdown_time'>{this.state.minutes}</div>
+              <div className='countdown_tag'>min</div>
+            </div>
+            <div className='countdown_item'>
+              <div className='countdown_time'>{this.state.seconds}</div>
+              <div className='countdown_tag'>sec</div>
+            </div>
+          </div>
         </div>
-      </div>
+      </Fade>
     );
   }
 }
