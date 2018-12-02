ad7b1f63de76b98e32939d795496303a2bea0af4
Deniz Mersinlioğlu <mersinliogludeniz@gmail.com>
2018-12-03 01:04:51 +0300

    Footer implemented

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import VenueInfo from './components/venue_info';
 import Highlights from './components/highlights';
 import Pricing from './components/pricing';
 import Location from './components/location';
+import Footer from './components/header_footer/Footer';
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
         <Highlights />
         <Pricing />
         <Location />
+        <Footer />
       </div>
     );
   }
